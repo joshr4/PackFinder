9380466a7f168554541fffe136eb51165936a85a
mc2147 <matthewchan2147@gmail.com>
2018-05-02 10:44:22 -0500

    semantic installed and Example.js template added

--- client/components/index.js
+++ client/components/index.js
@@ -5,4 +5,6 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
+export {default as SinglePark} from './SinglePark'
+export {default as Example} from './Example'
 export {Login, Signup} from './auth-form'
