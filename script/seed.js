0735e470b90d5efac32b3bba7a2506e51409694d
Daniel Simandl <dsimandl419@gmail.com>
2018-05-03 16:10:57 -0500

    seed file works for users and address

--- script/seed.js
+++ script/seed.js
@@ -9,80 +9,93 @@
  *
  * Now that you've got the main idea, check it out in practice below!
  */
-const db = require('../server/db')
-const {User, Pet, Park, Visit, Address, Event, Review} = require('../server/db/models')
+const db = require('../server/db');
+const {
+  User,
+  Pet,
+  Park,
+  Visit,
+  Address,
+  Event,
+  Review,
+} = require('../server/db/models');
+const usersSeed = require('./seed/seed-users');
+const parksSeed = require('./seed/seed-parks');
 
-async function seed () {
-  await db.sync({force: true})
-  console.log('db synced!')
+async function seed() {
+  await db.sync({ force: true });
+  console.log('db synced!');
+
+  console.log('seeding user');
+  await usersSeed();
+  await parksSeed();
   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
   // executed until that promise resolves!
   // Creating example seed addresses
-  const address1 = await Address.create({
-    line_1:"405 W Superior Street",
-    city: "Chicago",
-    state: "IL",
-    zip: "60615"
-  });
-  const addressLEDP = await Address.create({
-    line_1:"450 E Benton Pl",
-    city: "Chicago",
-    state: "IL",
-    zip: "60601"    
-  });
-  const addressOhioPlace = await Address.create({
-    line_1:"360 W Ohio St",
-    city: "Chicago",
-    state: "IL",
-    zip: "60654"    
-  });
-  // Creating example parks
-  const Park1 = await Park.create({
-    name: "Lakeshore East Dog Park",
-  });
-  const Park2 = await Park.create({
-    name: "Ohio Place Dog Park",
-  });
-  // Setting address of example parks
-  Park1.setAddress(addressLEDP);
-  await Park1.save();
-  Park2.setAddress(addressOhioPlace);
-  await Park2.save();
+  // const address1 = await Address.create({
+  //   line_1:"405 W Superior Street",
+  //   city: "Chicago",
+  //   state: "IL",
+  //   zip: "60615"
+  // });
+  // const addressLEDP = await Address.create({
+  //   line_1:"450 E Benton Pl",
+  //   city: "Chicago",
+  //   state: "IL",
+  //   zip: "60601"
+  // });
+  // const addressOhioPlace = await Address.create({
+  //   line_1:"360 W Ohio St",
+  //   city: "Chicago",
+  //   state: "IL",
+  //   zip: "60654"
+  // });
+  // // Creating example parks
+  // const Park1 = await Park.create({
+  //   name: "Lakeshore East Dog Park",
+  // });
+  // const Park2 = await Park.create({
+  //   name: "Ohio Place Dog Park",
+  // });
+  // // Setting address of example parks
+  // Park1.setAddress(addressLEDP);
+  // await Park1.save();
+  // Park2.setAddress(addressOhioPlace);
+  // await Park2.save();
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
-  const user1 = await User.create({email: 'user1@email.com', password:'user1'});
-  const user2 = await User.create({email: 'user2@email.com', password:'user2'});
-  const user3 = await User.create({email: 'user3@email.com', password:'user3'});
-  const startTime = new Date(2018, 4, 2, 16, 0);
-  const endTime = new Date(2018, 4, 2, 20, 0);
-  console.log("Park: ", Park);
-  // Adding Users to Park through "visits" MtM relationship 
-  await Park1.addUsers([user1, user2], {through: {
-    start: startTime,
-    end: endTime,
-  }})
-  await Park1.save();
-  // Adding Parks to Users through "visits" MtM relationship 
-  await user3.addParks([Park1, Park2], {through: {
-    start: startTime,
-    end: endTime,
-  }})
-  await user3.save();
-  // Adding a single park to a user through a "visit"
-  await user2.addParks(Park2, {through: {
-    start: startTime,
-    end: endTime,
-  }})
-  await user2.save();
-  
- 
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  // const users = await Promise.all([
+  //   User.create({email: 'cody@email.com', password: '123'}),
+  //   User.create({email: 'murphy@email.com', password: '123'})
+  // ])
+  // const user1 = await User.create({email: 'user1@email.com', password:'user1'});
+  // const user2 = await User.create({email: 'user2@email.com', password:'user2'});
+  // const user3 = await User.create({email: 'user3@email.com', password:'user3'});
+  // const startTime = new Date(2018, 4, 2, 16, 0);
+  // const endTime = new Date(2018, 4, 2, 20, 0);
+  // console.log("Park: ", Park);
+  // // Adding Users to Park through "visits" MtM relationship
+  // await Park1.addUsers([user1, user2], {through: {
+  //   start: startTime,
+  //   end: endTime,
+  // }})
+  // await Park1.save();
+  // // Adding Parks to Users through "visits" MtM relationship
+  // await user3.addParks([Park1, Park2], {through: {
+  //   start: startTime,
+  //   end: endTime,
+  // }})
+  // await user3.save();
+  // // Adding a single park to a user through a "visit"
+  // await user2.addParks(Park2, {through: {
+  //   start: startTime,
+  //   end: endTime,
+  // }})
+  // await user2.save();
+
+  // // Wowzers! We can even `await` on the right-hand side of the assignment operator
+  // // and store the result that the promise resolves to in a variable! This is nice!
+  // console.log(`seeded ${users.length} users`)
+  console.log(`seeded successfully`);
 }
 
 // Execute the `seed` function
@@ -90,19 +103,19 @@ async function seed () {
 // that might occur inside of `seed`
 seed()
   .catch(err => {
-    console.error(err.message)
-    console.error(err.stack)
-    process.exitCode = 1
+    console.error(err.message);
+    console.error(err.stack);
+    process.exitCode = 1;
   })
   .then(() => {
-    console.log('closing db connection')
-    db.close()
-    console.log('db connection closed')
-  })
+    console.log('closing db connection');
+    db.close();
+    console.log('db connection closed');
+  });
 
 /*
  * note: everything outside of the async function is totally synchronous
  * The console.log below will occur before any of the logs that occur inside
  * of the async function
  */
-console.log('seeding...')
+console.log('seeding...');
