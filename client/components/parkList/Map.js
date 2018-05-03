59b750f7b7cbe9540f0d2a99a24645097fd4ec8d
Ricky Li <ricky_0321@hotmail.com>
2018-05-03 10:42:27 -0500

    added onMoved listener to map

--- client/components/parkList/Map.js
+++ client/components/parkList/Map.js
@@ -4,19 +4,64 @@ import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 class Map extends Component {
 
+  constructor(){
+    super();
+    this.state = {
+      map: null,
+    }
+  }
+
+  mapMoved(){
+      console.log('mapMoved: ', JSON.stringify(this.state.map.getCenter()))
+    }
+
+    mapLoaded(map){
+      if (this.state.map !== null){
+        return
+      }
+
+      this.setState({ map })
+    }
+
+    zoomChanged(){
+      console.log('zoomChanged: ', this.state.map.getZoom())
+    }
+
+
+
   render() {
 
-    // const myKey = 'AIzaSyCcL9Cp8Qdi3dT9U5Iimud0LcDowumqomY';
-    const markers = this.props.markers || []
+    // const mapKey = 'AIzaSyCcL9Cp8Qdi3dT9U5Iimud0LcDowumqomY';
+    const markers = this.props.markers || [];
+
+    // <Marker
+    // position={{ lat: -34.397, lng: 150.644 }}
+    // />
+    // <Marker
+    // position={{lat:41.895266, lng:-87.6412237}}
+    // />
+
+
 
     return (
       <GoogleMap
+      ref={this.mapLoaded.bind(this)}
+      onDragEnd={this.mapMoved.bind(this)}
+      onZoomChanged={this.zoomChanged.bind(this)}
       defaultZoom={this.props.zoom}
       defaultCenter={this.props.center} >
+
       {markers.map((marker, index) => (
         <Marker {...marker} />
       )
     )}
+
+    <Marker
+    position={{ lat: -34.397, lng: 150.644 }}
+    />
+    <Marker
+    position={{lat:41.895266, lng:-87.6412237}}
+    />
     </GoogleMap>
     );
   }
