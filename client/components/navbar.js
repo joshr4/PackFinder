3a30fbc4b707654da89c811440d761035ce9eb61
Daniel Simandl <dsimandl419@gmail.com>
2018-05-04 13:30:47 -0500

    making menu transparent

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,24 +4,24 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
 import { Menu, Image } from 'semantic-ui-react';
-// https://logomakr.com/7sbzeB
+// https://logomakr.com/4zlisz
 
 const styles = {
   image: {
     maxWidth: '357px',
-    height: '100px'
-  }
-}
+    height: '100px',
+  },
+};
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <Menu stackable>
+  <Menu stackable style={{ backgroundColor: 'rgba(0,0,0,0)' }} borderless>
     <Image style={styles.image} src={'/images/logo.png'} />
     {isLoggedIn ? (
       <Menu.Menu position="right">
         {/* The navbar will show these links after you log in */}
         <Menu.Item as={Link} name="home" to="/home" />
         <Menu.Item onClick={handleClick} name="logout" to="/home" />
-        </Menu.Menu>
+      </Menu.Menu>
     ) : (
       <Menu.Menu position="right">
         {/* The navbar will show these links before you log in */}
