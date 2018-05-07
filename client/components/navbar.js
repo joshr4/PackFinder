d22014d0793d98b68a815f2de80086925ef1bbd0
Daniel Simandl <dsimandl419@gmail.com>
2018-05-07 11:01:33 -0500

    subNavbar is rendering

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
-import { Menu, Image, Header } from 'semantic-ui-react';
+import { Menu, Image, Header, Grid } from 'semantic-ui-react';
+import { SubNavbar } from '.';
 // https://logomakr.com/4zlisz
 
 const styles = {
@@ -19,7 +20,7 @@ const styles = {
     zIndex: 1,
     width: '100vw',
     color: '#54B8BF',
-    hight: '85px'
+    height: '85px',
   },
   menuItem: {
     color: '#fff',
@@ -31,8 +32,7 @@ const styles = {
     fontFamily: 'Pacifico, cursive',
     fontSize: 50,
     color: '#55b9bf',
-    textShadow:
-      '2px 5px 6px rgba(0,0,0,0.2)',
+    textShadow: '2px 5px 6px rgba(0,0,0,0.2)',
     letterSpacing: '2px',
     fontWeight: 100,
     paddingTop: '10px',
@@ -41,31 +41,39 @@ const styles = {
 };
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <Menu stackable style={styles.menu}>
-    {/* <Image style={styles.image} src={'/images/logo.png'} /> */}
-    <Header as="h1" style={styles.titleText}>
-      {' '}
-      Pack Finder{' '}
-    </Header>
-    {isLoggedIn ? (
-      <Menu.Menu position="right" style={{ color: '#54B8BF' }}>
-        {/* The navbar will show these links after you log in */}
-        <Menu.Item as={Link} name="home" to="/home" />
-        <Menu.Item onClick={handleClick} name="logout" to="/home" />
-      </Menu.Menu>
-    ) : (
-      <Menu.Menu position="right">
-        {/* The navbar will show these links before you log in */}
-        <Menu.Item style={styles.menuItem} as={Link} name="login" to="/login" />
-        <Menu.Item
-          style={styles.menuItem}
-          as={Link}
-          name="signup"
-          to="/signup"
-        />
-      </Menu.Menu>
-    )}
-  </Menu>
+  <div>
+    <Menu stackable style={styles.menu}>
+      {/* <Image style={styles.image} src={'/images/logo.png'} /> */}
+      <Header as="h1" style={styles.titleText}>
+        {' '}
+        Pack Finder{' '}
+      </Header>
+      {isLoggedIn ? (
+        <Menu.Menu position="right" style={{ color: '#54B8BF' }}>
+          {/* The navbar will show these links after you log in */}
+          <Menu.Item as={Link} name="home" to="/home" />
+          <Menu.Item onClick={handleClick} name="logout" to="/home" />
+        </Menu.Menu>
+      ) : (
+        <Menu.Menu position="right">
+          {/* The navbar will show these links before you log in */}
+          <Menu.Item
+            style={styles.menuItem}
+            as={Link}
+            name="login"
+            to="/login"
+          />
+          <Menu.Item
+            style={styles.menuItem}
+            as={Link}
+            name="signup"
+            to="/signup"
+          />
+        </Menu.Menu>
+      )}
+    </Menu>
+    {isLoggedIn ? <SubNavbar /> : <div />}
+  </div>
 );
 
 /**
