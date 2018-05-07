384f3400b5cc1bb221d854768ce4c0887ee2d5c3
Daniel Simandl <dsimandl419@gmail.com>
2018-05-07 14:45:41 -0500

    added wrapper for parks list and made it sticky

--- client/components/parkList/ParkList.js
+++ client/components/parkList/ParkList.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import { Map, ParkListItem } from '../index.js'
+import { Map, ParkListItem } from '../index.js';
 import { connect } from 'react-redux';
-import { Grid, Header, Image, Rail, Segment, Sticky } from  'semantic-ui-react'
-import { getParksAddresses } from '../../store/index.js'
+import { Grid, Header, Image, Rail, Segment, Sticky } from 'semantic-ui-react';
+import { getParksAddresses } from '../../store/index.js';
 
 export class ParkList extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {}
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
   componentDidMount() {
-    this.props.getEveryAddresses()
+    this.props.getEveryAddresses();
   }
-
+  handleContextRef = contextRef => this.setState({ contextRef });
 
   render() {
+    const location = { lat: 41.895266, lng: -87.6412237 };
 
-    const location = {lat: 41.895266, lng: -87.6412237}
-
-    const markers = this.props.nearbyParks
-
+    const markers = this.props.nearbyParks;
+    const { contextRef } = this.state;
+    console.log('context Ref', contextRef)
     // const markers = [{
     //   location: {lat: 41.895266, lng: -87.641223}
     //   }, {
@@ -30,39 +29,40 @@ export class ParkList extends Component {
     // ]
 
     return (
-
-      <Grid columns={2}>
-
-      <Grid.Column width={11}>
-      <div className="ui one cards">
-
-      {this.props.nearbyParks.map(park => {
-         return <ParkListItem key={park.id} currentPark={park} history={this.props.history} />
-      })}
-
-
-    </div>
-      </Grid.Column>
-
-
-
-
-      <Grid.Column width={5}>
-      <Map
-        zoom={14}
-        center={location}
-        markers={markers}
-        containerElement={<div style={{ height: `80vh` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    </Grid.Column>
-    </Grid>
+      <div className="container" ref={this.handleContextRef}>
+        <Grid columns={2}>
+          <Grid.Column width={9}>
+            <div className="ui one cards">
+              {this.props.nearbyParks.map(park => {
+                return (
+                  <ParkListItem
+                    key={park.id}
+                    currentPark={park}
+                    history={this.props.history}
+                  />
+                );
+              })}
+            </div>
+          </Grid.Column>
+          <Grid.Column width={7}>
+            <Sticky context={contextRef} offset={130}>
+              <Map
+                zoom={14}
+                center={location}
+                markers={markers}
+                containerElement={<div style={{ height: `80vh` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              />
+            </Sticky>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return {nearbyParks: state.parkList};
+  return { nearbyParks: state.parkList };
 };
 
 const mapDispatch = dispatch => {
