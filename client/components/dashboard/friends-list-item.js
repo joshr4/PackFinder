5bf7ee783458d31536fdc206ecb75dd35c6b37af
Daniel Simandl <dsimandl419@gmail.com>
2018-05-11 17:00:06 -0500

    working on hooking up buttons

--- client/components/dashboard/friends-list-item.js
+++ client/components/dashboard/friends-list-item.js
@@ -24,30 +24,35 @@ export const FriendsListItem = props => {
   const { imageUrl, address, fullName, pets, id, submit } = props.item;
 
   return (
-    <Grid.Row columns={2} style={{padding: '1.5em 0px' }}>
+    <Grid.Row columns={2} style={{ padding: '1.5em 0px' }}>
       <Grid.Column width={4}>
         <img style={{ width: '60px', borderRadius: '6em' }} src={imageUrl} />
       </Grid.Column>
       <Grid.Column width={12}>
         <Grid>
-            <Grid.Row style={{ padding: '0' }}>
-              <Header as="a">{fullName}</Header>
-              {pets && <Image
+          <Grid.Row style={{ padding: '0' }}>
+            <Header as="a">{fullName}</Header>
+            {pets && (
+              <Image
                 style={{ height: '80px', width: '80px' }}
                 rounded
                 src={pets[0].imageUrls[0]}
-              />}
-            </Grid.Row>
-            <Grid.Row style={{ padding: '0' }}>
-              {address && address.location && <Label
-                icon="globe"
-                content={`${address.location.distance} mi away`}
-              />}
-              <Button onClick={() => submit(user.id, id )} size="tiny" name="add">
-                send request
-              </Button>
-            </Grid.Row>
-          </Grid>
+              />
+            )}
+          </Grid.Row>
+          <Grid.Row style={{ padding: '0' }}>
+            {address &&
+              address.location && (
+                <Label
+                  icon="globe"
+                  content={`${address.location.distance} mi away`}
+                />
+              )}
+            <Button onClick={() => submit(user.id, id)} size="tiny" name="add">
+              send request
+            </Button>
+          </Grid.Row>
+        </Grid>
       </Grid.Column>
     </Grid.Row>
   );
@@ -56,7 +61,7 @@ export const FriendsListItem = props => {
 /**
  * CONTAINER
  */
-const mapState = ({user}) => ({user}) 
+const mapState = ({ user }) => ({ user });
 
 const mapDispatch = dispatch => {
   return {
