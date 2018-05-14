77976ddadec518faa2c16c506caa9ac5d5355e67
Daniel Simandl <dsimandl419@gmail.com>
2018-05-14 11:35:58 -0500

    working on sockets

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,12 +1,17 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log(`A socket connection to the server 
+    console.log(`A socket connection to the server
     has been made: ${socket.id}`)
     socket.on('new-message', message => {
       console.log(("NEW-MESSAGE "
       + " RECEIVED IN SOCKET/INDEX, broadcast.emit: "), message);
       socket.broadcast.emit('new-message', message);
     });
+    socket.on('update-friend', friend => {
+      console.log('BACKEND UPDATE FRIEND', friend);
+      socket.broadcast.emit('update-friend', friend);
+    });
+
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
