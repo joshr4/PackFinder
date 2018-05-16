4626ce5f20fdb40cec7bc83aea80e7b505bb2d53
Ricky Li <ricky_0321@hotmail.com>
2018-05-15 22:14:38 -0500

    add default image for person

--- server/db/models/pet.js
+++ server/db/models/pet.js
@@ -20,7 +20,7 @@ const Pet = db.define('pet', {
     },
     imageUrls: {
         type: Sequelize.ARRAY(Sequelize.STRING),
-        defaultValue:[],
+        defaultValue: ['/images/default/defaultDog.jpg'],
     },
     weight: {
         type: Sequelize.INTEGER,
