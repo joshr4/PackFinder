041279ab444b603063d3325d919fec2d8fa06a97
Ricky Li <ricky_0321@hotmail.com>
2018-05-16 13:04:52 -0500

    add email toLowerCase

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -50,7 +50,7 @@ const User = db.define('user', {
   },
   imageUrl: {
     type: Sequelize.STRING,
-    defaultValue : '/images/default/defaultPerson.jpg'
+    defaultValue : '/images/default/default-user.png'
   },
   description: {
     type: Sequelize.TEXT,
